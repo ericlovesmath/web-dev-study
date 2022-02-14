4525e95d14cf5d7bb3ad1e1b8cf149911ffedc18
Eric Lee <ericlovesmath@gmail.com>
2022-02-13 20:24:49 -0800

    Minesweeper Flagging

--- minesweeper/src/app.ts
+++ minesweeper/src/app.ts
@@ -1,17 +1,24 @@
-import { createBoard } from './minesweeper.js';
+import { createBoard, flagTile } from './minesweeper.js';
 
 const BOARD_SIZE = 10;
 const NUMBER_OF_MINES = 10;
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
 const boardElem = document.querySelector('.board') as HTMLDivElement;
+const minesLeftText = document.querySelector('[data-mine-count]') as HTMLSpanElement;
 
 console.log(board)
 
 board.forEach(row => {
     row.forEach(tile => {
         boardElem.append(tile.elem);
+        tile.elem.addEventListener('click', () => {})
+        tile.elem.addEventListener('contextmenu', (e: Event) => {
+            flagTile(tile);
+            e.preventDefault();
+        })
     })
 })
 
 boardElem.style.setProperty("--size", BOARD_SIZE.toString());
+minesLeftText.textContent = NUMBER_OF_MINES.toString();
--- minesweeper/src/minesweeper.ts
+++ minesweeper/src/minesweeper.ts
@@ -11,12 +11,20 @@ interface pos {
     y: number;
 }
 
+interface tile {
+    elem: HTMLDivElement;
+    x: number;
+    y: number;
+    mine: boolean;
+    status: string;
+}
+
 export function createBoard(boardSize: number, numberOfMines: number) {
-    const board = [];
+    const board: tile[][] = [];
     const minePositions = getMinePositions(boardSize, numberOfMines);
-    console.log(minePositions);
+
     for (let x = 0; x < boardSize; x++) {
-        const row = []
+        const row: tile[] = []
         for (let y = 0; y < boardSize; y++) {
             const elem = document.createElement('div');
             elem.dataset.status = TILE_STATUSES.HIDDEN;
@@ -28,9 +36,9 @@ export function createBoard(boardSize: number, numberOfMines: number) {
                     return position.x == x && position.y == y;
                 }),
                 get status() {
-                    return elem.dataset.status;
+                    return elem.dataset.status!;
                 },
-                set status(value) { // TODO: Add typing here?
+                set status(value: string) {
                     this.elem.dataset.status = value;
                 }
             });
@@ -39,6 +47,20 @@ export function createBoard(boardSize: number, numberOfMines: number) {
     }
     return board;
 }
+
+export function flagTile(tile: tile) {
+    if (tile.status !== TILE_STATUSES.HIDDEN && tile.status !== TILE_STATUSES.FLAGGED) {
+        return;
+    }
+
+    if (tile.status === TILE_STATUSES.FLAGGED) {
+        tile.status = TILE_STATUSES.HIDDEN;
+    } else {
+        tile.status = TILE_STATUSES.FLAGGED;
+    }
+
+}
+
 
 function getMinePositions(boardSize: number, numberOfMines: number): pos[] {
 
